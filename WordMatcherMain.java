1615c52cfd10f8ef512a8ad60df797bb78ffb262
Declan <edgecdec@gmail.com>
2019-06-18 23:01:19 -0700

    Fixed timer.

--- WordMatcherMain.java
+++ WordMatcherMain.java
@@ -5,8 +5,11 @@ import java.io.*;
 
 // Plays a game of Word Matcher.
 public class WordMatcherMain {
+
+   private static long totalTime;
    
    public static void main(String[] args) throws FileNotFoundException {
+     totalTime = 0;
      System.out.println("Welcome to the Word Matcher game");
      System.out.println("Type \"STOP\" at any point to end the game.");
      
@@ -31,11 +34,13 @@ public class WordMatcherMain {
         while (notAnswered) {
            notAnswered = game.guess(console);
            long elapsed = System.currentTimeMillis() - startTime;
-           System.out.println("You've taken " + elapsed / 1000 + "." + elapsed % 10 + "s so far");
+           System.out.println("You took " + Math.round(elapsed / 100) / 10.0 + "s for that round");
+           totalTime += elapsed;
+           //Print total time in seconds rounded to nearest tenth
+           System.out.println("You've taken " + Math.round(totalTime / 100) / 10.0 + "s so far");
         }
         System.out.print("Do you want to play again?c(y/n): ");
         userResponse = console.next();
      }        
    }
 }
-	
